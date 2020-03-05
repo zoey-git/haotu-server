31517b6e10a957ab5356c026c7d2680e0ca37289
zhounie <iszhounie@aliyun.com>
2020-03-05 17:09:45 +0800

    添加七牛云

--- router/user.js
+++ router/user.js
@@ -1,7 +1,8 @@
 const KoaRouter = require('koa-router')
 const router = new KoaRouter()
-const { crawler } = require('../controller/crawler')
+const { crawler, downFile } = require('../controller/crawler')
 
 router.get('/', crawler)
+router.post('/down', downFile)
 
 module.exports = router.routes()
